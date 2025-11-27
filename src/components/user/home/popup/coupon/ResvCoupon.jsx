b25aeaf656b54db3ef436e24b8a1839acead5e56
joonsu <junsu6580@gmail.com>
2025-11-27 19:27:03 +0900

    fix: 예약하기 쿠폰 디테일 수정

--- src/components/user/home/popup/coupon/ResvCoupon.jsx
+++ src/components/user/home/popup/coupon/ResvCoupon.jsx
@@ -10,7 +10,11 @@ export default function ResvCoupon({ coupons }) {
   return (
     <div className="rounded-[10px] px-[20px] py-[18px] bg-sky-mid-s flex items-center gap-[40px]">
       <span className="text-[20px] font-[600]">쿠폰</span>
-      <span className="text-[18px] font-[400]">시즌 3 선예약 쿠폰 사용</span>
+      <div className="flex w-[380px] justify-between shrink-0">
+        <div className="flex gap-[7px]">
+          시즌3 선예약 쿠폰 <p className="text-5 font-[500]">1</p>매 사용
+        </div>
+      </div>
     </div>
   );
 }
